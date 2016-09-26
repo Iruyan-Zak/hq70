92d9a25edf28b62ee29a1867aec839ab328de5e3
kento-tajiri <k.denjiry@gmail.com>
2016-09-26 21:29:49 +0900

    time up

--- questions/22/Main.hs
+++ questions/22/Main.hs
@@ -1,3 +1,12 @@
 #!/usr/bin/env stack
 -- stack runghc
 
+itodenwa16 :: Int
+itodenwa16 = itodenwaC 16
+
+itodenwaC :: Int -> Int
+itodenwaC 2 = 1
+itodenwaC n = 
+
+totyuu :: Int -> Int
+totyuu 3 list = itodenwaC
