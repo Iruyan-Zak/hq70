d355cdd581d13e2bf8c0ae2e6bba53cef12d8b56
Iruyan_Zak <yuri.kazuna@gmail.com>
2016-10-18 22:48:16 +0900

    Q24 を修正

--- questions/24/Main.hs
+++ questions/24/Main.hs
@@ -1,24 +1,29 @@
 main = print solve
 
-mask = (replicate 7 False) ++ [True] ++ (replicate 7 False)
-
-solve :: Int
 solve =
-    let doubles = replicate 8 False
-        hoge' n = (fact $ 9 - n) * hoge n 7 doubles
-    in sum . (map  hoge') $ [0..4]
+    let scoreOf n = (countCombinationOfDoubleStrike n) * fact (9-n)
+    in sum $ map scoreOf [0..4]
 
-hoge :: Int -> Int -> [Bool] -> Int
-hoge 0 _ doubles
-    | doubles !! 0 && doubles !! 7 = 0
-    | otherwise = 1
-hoge n p doubles
-    | p < 0 = 0
-    | otherwise =
-        let mask' = take 8 $ drop p mask
-            doubles' = zipWith (||) doubles mask'
-        in (hoge (n-1) (p-2) doubles') + (hoge n (p-1) doubles)
+countCombinationOfDoubleStrike :: Int -> Int
+countCombinationOfDoubleStrike n =
+    let initialPanelBorderStatus = replicate 8 False
+        initialPanelBorderIndex = 7
+    in countCombinationOfDoubleStrike' n initialPanelBorderIndex initialPanelBorderStatus
+
+countCombinationOfDoubleStrike' :: Int      -- Rest count of double strike
+                                -> Int      -- Current index of panel borders
+                                -> [Bool]   -- Current status of panel borders
+                                -> Int      -- Return: Number of double strike patterns
+countCombinationOfDoubleStrike' 0 _ (True:True:_) = 0
+countCombinationOfDoubleStrike' 0 _ _ = 1
+countCombinationOfDoubleStrike' _ index _ | index < 0 = 0
+countCombinationOfDoubleStrike' n index status =
+        let mask' = take 8 $ drop index mask
+            status' = zipWith (||) status mask'
+        in (countCombinationOfDoubleStrike' (n-1) (index-2) status') + (countCombinationOfDoubleStrike' n (index-1) status)
+
+mask = cycle $ True : (replicate 7 False)
 
 fact :: Int -> Int
 fact 0 = 1
-fact n = n * fact (n - 1)
+fact n = n * fact (n-1)
